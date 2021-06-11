dfd505cc076d7e6e1075cc8d0e10d9f50c3e538f
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-06-11 20:02:29 +0200

    preofessional mubasher news section

--- public/js/owl.custom.js
+++ public/js/owl.custom.js
@@ -1,50 +1,5 @@
 $(document).ready(function () {
 
-    $(".news-carousel").owlCarousel({
-        margin: 20,
-        loop: true,
-        mouseDrag: true,
-        touchDrag: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        rtl: true,
-        responsive: {
-            0: {
-                items: 1.2
-            },
-            280: {
-                items: 1
-            },
-            400: {
-                items: 1.2
-            },
-            576: {
-                items: 2.1
-            },
-            768: {
-                items: 2.75
-            },
-            992: {
-                items: 2.1
-            },
-            1200: {
-                items: 3
-            },
-            1300: {
-                items: 3.1
-            },
-            1500: {
-                items: 3.75
-            },
-            1600: {
-                items: 4
-            }
-        },
-    });
-
     $(".company-tab-carousel").owlCarousel({
         margin: 0,
         center: true,
